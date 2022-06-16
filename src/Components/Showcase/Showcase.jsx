42d49a02788d1cad5de881d28ad50f54acaaab29
NickMezacapa <89874146+NickMezacapa@users.noreply.github.com>
2022-06-16 13:56:22 -0700

    add testimonials component

--- src/Components/Showcase/Showcase.jsx
+++ src/Components/Showcase/Showcase.jsx
@@ -6,10 +6,14 @@ import AnimationsMovie from '../../Images/earth-recording-threeJS.mov';
 import LoadTimes from '../../Images/Showcase/load-times.png';
 import AppImgs from '../../Images/Showcase/app-imgs3.png';
 import Responsive from '../../Images/Showcase/responsive.png';
+import Disney from './ClientLogos/dis-logo.png';
+import ESPN from './ClientLogos/espn-logo.png';
+import Quora from './ClientLogos/q-logo.png';
+import Shopify from './ClientLogos/shopify-logo.png';
 
 
 const Showcase = () => {
-    
+
 
     const handleScroll3 = () => {
         const trigger = document.querySelector('#dd-trigger');
@@ -18,7 +22,7 @@ const Showcase = () => {
         const viewport_height = window.innerHeight;
         if (trigger && months) {
             const triggerRect = trigger.getBoundingClientRect();
-           // once the top of trigger enters the viewport, add the active class
+            // once the top of trigger enters the viewport, add the active class
             if (triggerRect.top <= viewport_height) {
                 months.classList.add('dd-months-strike');
                 months.classList.add('dd-months-gray');
@@ -34,131 +38,153 @@ const Showcase = () => {
 
 
 
-  return (
-    <div className='showcaseCta'>
-        <div className="overflow-container">
-        <section className="showcase-animations">
-            <div className="animations-description">
-                <div className="animations-heading">
-                    <h2>Stunning<br />Animations.</h2>
-                    </div>
-                <div className="animations-subheading">
-                    Whether it's subtle interactions that improve the UX, or complex "WOW" animations, your wish is my command.
-                </div>
-            </div>
-            <div className="animations-hero">
-                <img src={ BlankHero } alt="Animations display" id='blankhero-img' />
-                
-                <div className="animations-heroContent">
-                    
-                    <Plx
-             parallaxData={[
-                {
-/*                   start: "3035px",
-                  end: "3195px", */
-                  start: "self",
-                  duration: "300px",
-                  easing: "ease-in",
-                  properties: [
-                    {
-                      startValue: 1,
-                      endValue: 0,
-                      property: "opacity",
-                    },
-                  ],
-                },
-              ]}
-              style={{
-                height: "100%",
-                width: "100%",
-                zIndex: 3,
-                objectFit: "cover",
-                overflow: "hidden",
-                backgroundColor: "rgba(0, 0, 0, 1)",
-                willChange: "opacity",
-                opacity: 1,
-                position: "absolute",
-                top: '0.25rem'
-              }}
-            >
-                    <div className="overlay"></div>
-                    </Plx>
-                    <video muted autoPlay loop src={ AnimationsMovie }></video>
-                </div>
-                
-
-            </div>
-        </section>
-        <section className="showcase1">
-            <div className="loadTimes">
-                <h2 className="fastDelivery">
-                    Fast Delivery.<br />
-                    Faster load times.
-                </h2>
-                <img src={LoadTimes} loading="lazy" sizes="(max-width: 479px) 86vw, (max-width: 767px) 79vw, 600px" alt="Screenshot of a custom website showing a performance score of 100 points" id='loadTimes-img'></img>
-            </div>
-            <div className="latest-tech">
-                <img src={AppImgs} alt="Technology used by Nick Mezacapa" />
-                <h2>The latest technology.</h2>
-            </div>
-        </section>
-        <section className="showcase2">
-            <div className="showcase-small">
-                <div className="dev-duration">
-                    <h2>Development Duration</h2>
-                    <div className="dd-time-cta">
-                    <h3 className="dd-months">Months</h3>
-                    <h3 id="dd-weeks">Weeks</h3>
-                    </div>
-                </div>
-                <div className="clean-code">
-                    <h2 id='dd-trigger'>Clean Code</h2>
-                    <div className="clean-code-content">
-                        <span className="bracket">&#60;</span>
-                        <span className="identifier">main</span>
-                        <span className="bracket">&#62;</span><br />
-                        <div className="indent">
-                        <span className="bracket">&#60;</span>
-                        <span className="identifier">article </span>
-                        <span className="class">class</span>
-                        <span className="bracket">=</span>
-                        <span className="class-content">"firstArticle"</span>
-                        <span className="bracket">&#62;</span><br />
-                        <div className="indent2">
-                        <span className="bracket">&#60;</span>
-                        <span className="identifier">h1</span>
-                        <span className="bracket">&#62;</span>Simple, Semantic, Testable
-                        <span className="bracket">&#60;/</span>
-                        <span className="identifier">h1</span>
-                        <span className="bracket">&#62;</span><br />
-                        <span className="bracket">&#60;</span>
-                        <span className="identifier">p</span>
-                        <span className="bracket">&#62;</span>Clearly communicated code.
-                        <span className="bracket">&#60;/</span>
-                        <span className="identifier">p</span>
-                        <span className="bracket">&#62;</span><br />
+    return (
+        <div className='showcaseCta'>
+            <div className="overflow-container">
+                <section className="showcase-animations">
+                    <div className="animations-description">
+                        <div className="animations-heading">
+                            <h2>Stunning<br />Animations.</h2>
                         </div>
-                        <span className="bracket">&#60;/</span>
-                        <span className="identifier">article</span>
-                        <span className="bracket">&#62;</span><br />
+                        <div className="animations-subheading">
+                            Whether it's subtle interactions that improve the UX, or complex "WOW" animations, your wish is my command.
                         </div>
-                        <span className="bracket">&#60;/</span>
-                        <span className="identifier">main</span>
-                        <span className="bracket">&#62;</span>
+                    </div>
+                    <div className="animations-hero">
+                        <img src={BlankHero} alt="Animations display" id='blankhero-img' />
+
+                        <div className="animations-heroContent">
+
+                            <Plx
+                                parallaxData={[
+                                    {
+                                        /*                   start: "3035px",
+                                                          end: "3195px", */
+                                        start: "self",
+                                        duration: "300px",
+                                        easing: "ease-in",
+                                        properties: [
+                                            {
+                                                startValue: 1,
+                                                endValue: 0,
+                                                property: "opacity",
+                                            },
+                                        ],
+                                    },
+                                ]}
+                                style={{
+                                    height: "100%",
+                                    width: "100%",
+                                    zIndex: 3,
+                                    objectFit: "cover",
+                                    overflow: "hidden",
+                                    backgroundColor: "rgba(0, 0, 0, 1)",
+                                    willChange: "opacity",
+                                    opacity: 1,
+                                    position: "absolute",
+                                    top: '0.25rem'
+                                }}
+                            >
+                                <div className="overlay"></div>
+                            </Plx>
+                            <video muted autoPlay loop src={AnimationsMovie}></video>
+                        </div>
 
 
                     </div>
+                </section>
+                <section className="showcase1">
+                    <div className="loadTimes">
+                        <h2 className="fastDelivery">
+                            Fast Delivery.<br />
+                            Faster load times.
+                        </h2>
+                        <img src={LoadTimes} loading="lazy" sizes="(max-width: 479px) 86vw, (max-width: 767px) 79vw, 600px" alt="Screenshot of a custom website showing a performance score of 100 points" id='loadTimes-img'></img>
+                    </div>
+                    <div className="latest-tech">
+                        <img src={AppImgs} alt="Technology used by Nick Mezacapa" />
+                        <h2>The latest technology.</h2>
+                    </div>
+                </section>
+                <section className="showcase2">
+                    <div className="showcase-small">
+                        <div className="dev-duration">
+                            <h2>Development Duration</h2>
+                            <div className="dd-time-cta">
+                                <h3 className="dd-months">Months</h3>
+                                <h3 id="dd-weeks">Weeks</h3>
+                            </div>
+                        </div>
+                        <div className="clean-code">
+                            <h2 id='dd-trigger'>Clean Code</h2>
+                            <div className="clean-code-content">
+                                <span className="bracket">&#60;</span>
+                                <span className="identifier">main</span>
+                                <span className="bracket">&#62;</span><br />
+                                <div className="indent">
+                                    <span className="bracket">&#60;</span>
+                                    <span className="identifier">article </span>
+                                    <span className="class">class</span>
+                                    <span className="bracket">=</span>
+                                    <span className="class-content">"firstArticle"</span>
+                                    <span className="bracket">&#62;</span><br />
+                                    <div className="indent2">
+                                        <span className="bracket">&#60;</span>
+                                        <span className="identifier">h1</span>
+                                        <span className="bracket">&#62;</span>Simple, Semantic, Testable
+                                        <span className="bracket">&#60;/</span>
+                                        <span className="identifier">h1</span>
+                                        <span className="bracket">&#62;</span><br />
+                                        <span className="bracket">&#60;</span>
+                                        <span className="identifier">p</span>
+                                        <span className="bracket">&#62;</span>Clearly communicated code.
+                                        <span className="bracket">&#60;/</span>
+                                        <span className="identifier">p</span>
+                                        <span className="bracket">&#62;</span><br />
+                                    </div>
+                                    <span className="bracket">&#60;/</span>
+                                    <span className="identifier">article</span>
+                                    <span className="bracket">&#62;</span><br />
+                                </div>
+                                <span className="bracket">&#60;/</span>
+                                <span className="identifier">main</span>
+                                <span className="bracket">&#62;</span>
+
+
+                            </div>
+                        </div>
+                    </div>
+                    <div className="responsive">
+                        <h2>Responsive<br />
+                            Design.</h2>
+                        <img src={Responsive} alt="an example of screen friendly development" />
+                    </div>
+                </section>
+                <div className="Showcase-Bottom_cta">
+                    <div className="Showcase-Small_clients">
+                        <h2 className="clients-title">
+                            Clients that speak for themselves
+                        </h2>
+                        <div className="Showcase-Client_logos">
+                            <div className="disney-logo cl-cta">
+                                <img src={Disney} alt="logo" />
+                            </div>
+                            <div className="espn-logo cl-cta">
+                                <img src={ESPN} alt="logo" />
+                            </div>
+                            <div className="quora-logo cl-cta">
+                                <img src={Quora} alt="logo" />
+                            </div>
+                            <div className="shopify-logo cl-cta">
+                                <img src={Shopify} alt="logo" />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="Showcase-Long_testimonials"></div>
                 </div>
             </div>
-            <div className="responsive">
-                <h2>Responsive<br />
-                Design.</h2>
-                <img src={Responsive} alt="an example of screen friendly development" />
-            </div>
-        </section>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Showcase
